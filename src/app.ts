6dfedddb3f840622a3ff807f389bba285bb92ebe
emazurov <emazurov@ozon.ru>
2020-04-12 20:07:44 +0300

    : add : add bodyparser, add cors

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import http, { Server } from 'http';
 import Koa from 'koa';
 import KoaRouter from 'koa-router';
+import cors from '@koa/cors';
 import { Game } from './game';
+import bodyParser from 'koa-bodyparser';
 
 class App {
   constructor() {
@@ -16,6 +18,27 @@ class MockServer {
   constructor() {
     const app = new Koa();
     const router = new KoaRouter();
+
+    app.use(
+      cors({
+        origin: '*',
+        credentials: true,
+      }),
+    );
+    app.use(bodyParser());
+    app.use(async (ctx, next) => {
+      ctx.set(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, X-Requested-With, x-access-token',
+      );
+      ctx.set('Access-Control-Allow-Methods', 'POST, GET, PUT, DELETE, OPTIONS');
+
+      if (ctx.method === 'OPTIONS') {
+        return (ctx.status = 200);
+      }
+
+      await next();
+    });
 
     app.use(router.routes());
     app.use(router.allowedMethods());
