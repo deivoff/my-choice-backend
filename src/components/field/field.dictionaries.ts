c9d48574f654a4d0fc51daabad0e0bc93db0365f
Amina Mazurova <amina.rabadanova42@gmail.com>
2020-04-20 20:29:16 +0300

    add offers and reaction

--- src/components/field/field.dictionaries.ts
+++ src/components/field/field.dictionaries.ts
@@ -616,6 +616,509 @@ const OFFERS: Offer[] = [
       }
     ]
   },
+  {
+    id: 2,
+    type: FieldType.offer,
+    description: "Незнакомец попросил передать сверток в другой город. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          lives: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь только за деньги",
+        resources: {
+          lives: -1,
+          money: 10,
+        }
+      },
+      {
+        id: 2,
+        text: "Откажетесь и сообщите в полицию",
+        resources: {
+          white: 1,
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь",
+        resources: {}
+      }
+    ]
+  },
+  {
+    id: 3,
+    type: FieldType.offer,
+    description: "Вас попросили поучаствовать в благотворительности - почитать слепым. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          white: 1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь только за деньги",
+        resources: {
+          money: 10,
+        }
+      },
+      {
+        id: 2,
+        text: "Согласитесь, но не придете",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь",
+        resources: {}
+      }
+    ]
+  },
+  {
+    id: 4,
+    type: FieldType.offer,
+    description: "Вам предложили стать волонтером на международном Фестивале. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          white: 1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь только за деньги",
+        resources: {
+          money: 10,
+        }
+      },
+      {
+        id: 2,
+        text: "Согласитесь, но не придете на мероприятие",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь",
+        resources: {}
+      }
+    ]
+  },
+  {
+    id: 5,
+    type: FieldType.offer,
+    description: "Попросили сыграть в спектакле для детского дома. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          white: 1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь только за деньги",
+        resources: {
+          money: 10,
+        }
+      },
+      {
+        id: 2,
+        text: "Согласитесь, но не придете",
+        resources: {
+          dark: -1,
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь",
+        resources: {}
+      }
+    ]
+  },
+  {
+    id: 6,
+    type: FieldType.offer,
+    description: "У Вас есть супер идея проекта. Пойдете ли подавать на грантовый конкурс?",
+    choices: [
+      {
+        id: 0,
+        text: "Да",
+        resources: {
+          white: 1,
+          money: 10,
+        }
+      },
+      {
+        id: 1,
+        text: "Нет",
+        resources: {
+          
+        }
+      }
+    ]
+  },
+  {
+    id: 7,
+    type: FieldType.offer,
+    description: "Бездомный на улице подошел и попросился переночевать у Вас. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          white: 1,
+          lives: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь, но потребуете что-то взамен",
+        resources: {
+          lives: -1,
+          money: 10,
+        }
+      },
+      {
+        id: 2,
+        text: "Молча проигнорируете",
+        resources: {
+        }
+      },
+      {
+        id: 3,
+        text: "Откажете в грубой форме",
+        resources: {
+          white: -1,
+        }
+      }
+    ]
+  },
+  {
+    id: 8,
+    type: FieldType.offer,
+    description: "В магазине Вам дали сдачу больше, чем должны были. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Взяли и молча ушли",
+        resources: {
+          money: 10,
+        }
+      },
+      {
+        id: 1,
+        text: "Сообщили об ошибке и вернули деньги",
+        resources: {
+          white: 1,
+        }
+      }
+      
+    ]
+  },
+  {
+    id: 9,
+    type: FieldType.offer,
+    description: "Незнакомец попросил покараулить дорогие вещи. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          white: 1,
+          lives: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь только за деньги",
+        resources: {
+          lives: -1,
+          money: 10,
+        }
+      },
+      {
+        id: 2,
+        text: "Согласитесь и украдете",
+        resources: {
+          dark: 1,
+          money: 50,
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь",
+        resources: {
+          lives: 1,
+        }
+      }
+    ]
+  },
+  {
+    id: 10,
+    type: FieldType.offer,
+    description: "Вам предложили покурить «травку». Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          dark: 1,
+          money: -10,
+          lives: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Откажетесь и уйдете",
+        resources: {
+        }
+      },
+      {
+        id: 2,
+        text: "Откажетесь, но продолжите общаться",
+        resources: {
+          lives: -1,
+          white: -1,
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь и сообщите в полицию",
+        resources: {
+          white: 1,
+        }
+      }
+    ]
+  },
+  {
+    id: 11,
+    type: FieldType.offer,
+    description: "Вам предложили «постоять на шухере» во время кражи. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          dark: 1,
+          lives: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь только за деньги",
+        resources: {
+          dark: 1,
+          money: 10,
+          lives: -1,
+        }
+      },
+      {
+        id: 2,
+        text: "Откажетесь",
+        resources: {
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь и сообщите в полицию",
+        resources: {
+          white: 1,
+        }
+      }
+    ]
+  },
+  {
+    id: 12,
+    type: FieldType.offer,
+    description: "Вас попросили побыть сиделкой. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          white: 1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь только за деньги",
+        resources: {
+          money: 10,
+        }
+      },
+      {
+        id: 2,
+        text: "Согласитесь, но не придете",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь",
+        resources: {
+          
+        }
+      }
+    ]
+  },
+  {
+    id: 13,
+    type: FieldType.offer,
+    description: "Вас попросили почистить питомник для животных. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          white: 1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь только за деньги",
+        resources: {
+          money: 10,
+        }
+      },
+      {
+        id: 2,
+        text: "Согласитесь, но не придете",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь",
+        resources: {
+          
+        }
+      }
+    ]
+  },
+  {
+    id: 14,
+    type: FieldType.offer,
+    description: "Вам предложили съесть собаку по-фану. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          dark: 1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь только за деньги",
+        resources: {
+          money: 10,
+          dark: 1,
+        }
+      },
+      {
+        id: 2,
+        text: "Откажетесь и сообщите в полицию",
+        resources: {
+          white: 1,
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь",
+        resources: {
+          
+        }
+      }
+    ]
+  },
+  {
+    id: 15,
+    type: FieldType.offer,
+    description: "Добровольцы попросили денег на благотворительный проект. Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          white: 3,
+          money: -10,
+        }
+      },
+      {
+        id: 1,
+        text: "Откажете",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 2,
+        text: "Откажете и отберете деньги",
+        resources: {
+          money: 10,
+          dark: 1,
+        }
+      },
+      {
+        id: 3,
+        text: "Молча проигнорируете",
+        resources: {
+          
+        }
+      }
+    ]
+  },
+  {
+    id: 16,
+    type: FieldType.offer,
+    description: "Вам предложили сбежать из дома «в общину». Ваша реакция:",
+    choices: [
+      {
+        id: 0,
+        text: "Согласитесь",
+        resources: {
+          lives:-1,
+          dark: 1,
+        }
+      },
+      {
+        id: 1,
+        text: "Согласитесь по-фану на недельку",
+        resources: {
+          lives:-1,
+        }
+      },
+      {
+        id: 2,
+        text: "Откажетесь",
+        resources: {
+        }
+      },
+      {
+        id: 3,
+        text: "Откажетесь и сообщите в полицию",
+        resources: {
+          white: 1,
+        }
+      }
+    ]
+  },
 ];
 
 const INCIDENTS: Incident[] = [
@@ -656,6 +1159,7 @@ const OPPORTUNITIES: Opportunity[] = [
 ];
 
 const REACTIONS: Reaction[] = [
+ 
   {
     id: 0,
     type: FieldType.reaction,
@@ -680,7 +1184,7 @@ const REACTIONS: Reaction[] = [
       },
     ]
   },
-  {
+    {
     id: 1,
     type: FieldType.reaction,
     description: "В гостях Вы нечаянно разбили вазу.",
@@ -700,6 +1204,367 @@ const REACTIONS: Reaction[] = [
       {
         id: 2,
         text: "Извинюсь и постараюсь возместить ущерб",
+        resources: {}
+      },
+    ]
+  }, 
+    {
+    id: 2,
+    type: FieldType.reaction,
+    description: "Отчислили из школы/ВУЗа/уволили с работы.",
+    choices: [
+      {
+        id: 0,
+        text: "Уйду в депрессию и буду всех обвинять",
+        resources: {
+          lives: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Постараюсь заново поступить или устроиться",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Оставлю все как есть – само разрешится",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 3,
+    type: FieldType.reaction,
+    description: "В гардеробе кому-то отдали Вашу куртку.",
+    choices: [
+      {
+        id: 0,
+        text: "Устрою скандал",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Выясню ситуацию и постараюсь помочь персоналу найти куртку",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Напишу заявление в полицию",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 4,
+    type: FieldType.reaction,
+    description: "Вас обрызгала машина.",
+    choices: [
+      {
+        id: 0,
+        text: "Целый день буду ходить мрачнее тучи и срываться на всех.",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Отряхнусь и забуду об этом недоразумении.",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Обращусь в администрацию, чтобы ликвидировали лужу.",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 5,
+    type: FieldType.reaction,
+    description: "Вас нечаянно, но сильно толкнули.",
+    choices: [
+      {
+        id: 0,
+        text: "Толкну в ответ, устрою «разборку».",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Вежливо попрошу извинений.",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Промолчу",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 6,
+    type: FieldType.reaction,
+    description: "Вы разбили телефон.",
+    choices: [
+      {
+        id: 0,
+        text: "Целый день буду ходить мрачнее тучи и срываться на всех.",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Постараюсь починить телефон, буду спокойно решать проблему.",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Оставлю все как есть – и без телефона хорошо",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 7,
+    type: FieldType.reaction,
+    description: "Вам недодали сдачу в магазине.",
+    choices: [
+      {
+        id: 0,
+        text: "Молча уйду.",
+        resources: {
+          lives: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Устрою большой скандал.",
+        resources: {
+            white: -1,
+        }
+      },
+      {
+        id: 2,
+        text: "Напишу заявление в Роспотребнадозр.",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 8,
+    type: FieldType.reaction,
+    description: "Кот сходил в туалет в Ваши ботинки.",
+    choices: [
+      {
+        id: 0,
+        text: "Это было последнее, что сделал кот в своей жизни.",
+        resources: {
+          dark: 1,
+        }
+      },
+      {
+        id: 1,
+        text: "Уберу и буду спокоен.",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Целый день буду ходить мрачнее тучи и срываться на всех.",
+        resources: {
+            white: -1
+        }
+      },
+    ]
+  },
+    {
+    id: 9,
+    type: FieldType.reaction,
+    description: "Ваши друзья уехали тусить, а Вас не позвали.",
+    choices: [
+      {
+        id: 0,
+        text: "Больше никогда не буду с ними общаться.",
+        resources: {
+          lives: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Узнаю в чем дело и объясню, что они задели меня.",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Промолчу.",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 10,
+    type: FieldType.reaction,
+    description: "Вы приехали на другой конец города в магазин, а он закрыт.",
+    choices: [
+      {
+        id: 0,
+        text: "Целый день буду ходить мрачнее тучи и срываться на всех.",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Приеду в этот магазин в следующий раз, предварительно позвонив.",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Не нужен мне этот товар – больше не буду ездить.",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 11,
+    type: FieldType.reaction,
+    description: "Кондуктор в автобусе прошел мимо Вас, не взяв деньги за проезд.",
+    choices: [
+      {
+        id: 0,
+        text: "Сделаю вид, что все «так и надо»",
+        resources: {
+          white: -1,
+        }
+      },
+      {
+        id: 1,
+        text: "Остановлю кондуктора и заплачу.",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Остановлю кондуктора и заплачу.",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 12,
+    type: FieldType.reaction,
+    description: "Вы поймали «за руку» карманника, который хотел у вас украсть кошелек/телефон.",
+    choices: [
+      {
+        id: 0,
+        text: "Вызову полицию",
+        resources: {
+          white: 1,
+        }
+      },
+      {
+        id: 1,
+        text: "Сам «разберусь» и проучу.",
+        resources: {
+            dark: 1
+        }
+      },
+      {
+        id: 2,
+        text: "Испугаюсь и сам убегу.",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 13,
+    type: FieldType.reaction,
+    description: "В магазине вам продали просроченный товар.",
+    choices: [
+      {
+        id: 0,
+        text: "Молча уйду.",
+        resources: {}
+      },
+      {
+        id: 1,
+        text: "Устрою большой скандал.",
+        resources: {
+            white: -1
+        }
+      },
+      {
+        id: 2,
+        text: "Напишу заявление в Роспотребнадозр",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 14,
+    type: FieldType.reaction,
+    description: "Ваш лучший друг не поздравил Вас с Днем рождения.",
+    choices: [
+      {
+        id: 0,
+        text: "Больше никогда не буду с ними общаться",
+        resources: {
+            white: -1
+        }
+      },
+      {
+        id: 1,
+        text: "Узнаю в чем дело и объясню, что они задели меня.",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Промолчу",
+        resources: {}
+      },
+    ]
+  },
+    {
+    id: 15,
+    type: FieldType.reaction,
+    description: "Вы почти закончили собирать паззл на 3000 деталей и его разобрали у вас на глазах.",
+    choices: [
+      {
+        id: 0,
+        text: "Ну окей",
+        resources: {}
+      },
+      {
+        id: 1,
+        text: "Негативная реакция.",
+        resources: {
+            dark: 1
+        }
+      },
+    ]
+  },
+    {
+    id: 16,
+    type: FieldType.reaction,
+    description: "Вас бросил(а) парень (девушка).",
+    choices: [
+      {
+        id: 0,
+        text: "Целый день буду ходить мрачнее тучи и срываться на все.",
+        resources: {
+            lives: 1
+        }
+      },
+      {
+        id: 1,
+        text: "Забуду об этом недоразумении.",
+        resources: {}
+      },
+      {
+        id: 2,
+        text: "Начну заново.",
         resources: {}
       },
     ]
