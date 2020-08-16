fabc59e0e3f8535e1ecc09c814ea3afa310eb62b
emazurov <emazurov@ozon.ru>
2020-08-16 16:34:17 +0300

    : add : add video to question 1-20

--- src/components/question/question.router.ts
+++ src/components/question/question.router.ts
@@ -1,9 +1,11 @@
 import Router from 'koa-router';
+import data from './data/data.json';
 import { QuestionModel } from '$components/question';
 
 const questionRouter = new Router();
 
 questionRouter.get('/questions', async (ctx, next) => {
+  // QuestionModel.insertMany(data);
   ctx.body = await QuestionModel.find({}, {
     correct: 0,
     _id: 0,
