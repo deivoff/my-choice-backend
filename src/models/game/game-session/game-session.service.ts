e608d3e7f90a31fd10ede7ab8344622ffbad15f2
emazurov <emazurov@ozon.ru>
2021-05-12 20:50:47 +0300

    : fix : scan game keys

--- src/models/game/game-session/game-session.service.ts
+++ src/models/game/game-session/game-session.service.ts
@@ -48,6 +48,7 @@ export class GameSessionService {
   private async getAllKeys(cursor: string = '0', keys: string[] = []): Promise<string[]> {
     const [newCursor, newKeys] = await this.redisClient.scan(cursor, 'match', this.key('*'));
     keys.concat(newKeys);
+    console.log({ newCursor });
     if (newCursor && newCursor === '0') {
       return keys;
     }
