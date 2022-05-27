e340535381cf4cf86c829776d40e74ea96de6625
deivoff <deivoff@gmail.com>
2022-05-27 19:44:58 +0500

    added ci for project

---
+++ bin/deployment.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+yarn install --production
+
+NODE_ENV=$1
+PORT=$2
+ORIGIN_URL=$3
+
+DB_NAME=$4
+DB_PASS=$5
+DB_USER=$6
+DB_URI=$7
+
+SECRET_ACCESS_KEY=$8
+SECRET_REFRESH_KEY=$9
+
+VK_CLIENT_ID=${10}
+VK_CLIENT_SECRET=${11}
+
+REDIS_URL=${12}
+SSL=${13}
+
+SENTRY_DSN=${14}
+
+echo "module.exports = {
+  apps: [{
+    name: 'MyChoiceBackend',
+    port: '$PORT',
+    script: '$PWD/build/index.js',
+    env: {
+      NODE_ENV: '$NODE_ENV',
+      PORT: '$PORT',
+      ORIGIN_URL: '$ORIGIN_URL',
+      DB_NAME: '$DB_NAME',
+      DB_URI: '$DB_URI',
+      DB_USER: '$DB_USER',
+      DB_PASS: '$DB_PASS',
+      SECRET_ACCESS_KEY: '$SECRET_ACCESS_KEY',
+      SECRET_REFRESH_KEY: '$SECRET_REFRESH_KEY',
+      VK_CLIENT_ID: '$VK_CLIENT_ID',
+      VK_CLIENT_SECRET: '$VK_CLIENT_SECRET',
+      REDIS_URL: '$REDIS_URL',
+      SSL: '$SSL',
+      SENTRY_DSN: '$SENTRY_DSN',
+    },
+  }],
+};" >| ecosystem.config.js
